0ea3a8f20ba6c67b51094fcc50aa1884481c8e30
ConstantDust <90234538+ConstantDust@users.noreply.github.com>
2023-01-14 20:39:18 -0500

    GravityGun

--- common/src/main/kotlin/org/valkyrienskies/tournament/tournamentItems.kt
+++ common/src/main/kotlin/org/valkyrienskies/tournament/tournamentItems.kt
@@ -1,16 +1,19 @@
 package org.valkyrienskies.tournament
 
 import net.minecraft.core.Registry
-import net.minecraft.resources.ResourceLocation
-import net.minecraft.world.item.CreativeModeTab
 import net.minecraft.world.item.Item
-import net.minecraft.world.item.ItemStack
-import org.valkyrienskies.tournament.registry.CreativeTabs
+import org.valkyrienskies.tournament.item.GrabGun
+import org.valkyrienskies.tournament.item.GrabGunOG
 import org.valkyrienskies.tournament.registry.DeferredRegister
 
 @Suppress("unused")
 object tournamentItems {
     private val ITEMS = DeferredRegister.create(tournamentMod.MOD_ID, Registry.ITEM_REGISTRY)
+
+    val GRABGUNOG = ITEMS.register("grabgun_old", ::GrabGunOG)
+    val GRABGUN = ITEMS.register("grabgun", ::GrabGun)
+
+
 //    val TAB: CreativeModeTab = CreativeTabs.create(
 //        ResourceLocation(
 //            tournamentMod.MOD_ID,
